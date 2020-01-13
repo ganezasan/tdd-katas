89db6bf5adb6bbcc6c77edf440ff68e66640c59b
ganezasan <istaka12@gmail.com>
2020-01-13 15:53:45 +0900

    add Greeter
    To learn tdd process, some below features added
    - say hello and name
    - trims the imput
    - capitalizes the first letter of the name
    - Good morning <name> when the time is 6:00-12:00

--- src/Greeter/Greeter.js
+++ src/Greeter/Greeter.js
@@ -1,12 +1,19 @@
-const _ = require('lodash');
+function _capitalize(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1, str.length);
+}
 
 class Greeter {
   constructor() {
   }
 
   greet(name) {
-    return "yo yo yo";
+    const trimmedName = name.trim();
+    const capitalizedName = _capitalize(trimmedName);
+    const hours = new Date().getHours();
+    const greetMessage = hours >= 6 && hours < 12 ? 'Good morning' : 'Hello';
+    return `${greetMessage} ${capitalizedName}!`;
   }
 }
 
 module.exports = Greeter;
+
--- src/Greeter/Greeter.test.js
+++ src/Greeter/Greeter.test.js
@@ -2,12 +2,34 @@ const Greeter = require('./Greeter');
 
 describe('Greeter', () => {
   let greeter;
+  const noonTime = new Date('2019-12-25T13:00:00');
+  const morningTime = new Date('2019-12-25T06:00:00');
 
   beforeEach(() => {
+    global.Date = jest.fn(() => noonTime);
     greeter = new Greeter();
+  });
+  afterEach(() => {
+    global.Date.mockRestore()
   });
 
   it('says Hello and name', () => {
     expect(greeter.greet('Daniel')).toEqual('Hello Daniel!');
+    expect(greeter.greet('Taka')).toEqual('Hello Taka!');
+  });
+
+  it('trims the input', () => {
+    expect(greeter.greet('   Daniel ')).toEqual('Hello Daniel!');
+    expect(greeter.greet('　マレック')).toEqual('Hello マレック!');
+  });
+
+  it('capitalizes the first letter of the name', () => {
+    expect(greeter.greet('daNiel')).toEqual('Hello DaNiel!');
+    expect(greeter.greet('d')).toEqual('Hello D!');
+  });
+
+  it('Good morning <name> when the time is 06:00-12:00', () => {
+    global.Date = jest.fn(() => morningTime);
+    expect(greeter.greet('daniel')).toEqual('Good morning Daniel!');
   });
 });
